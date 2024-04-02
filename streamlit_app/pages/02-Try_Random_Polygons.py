aa11c0d6f635df5706093a6920419426f3bf82e9
Connor Ferster <connorferster@gmail.com>
2024-04-01 23:14:59 -0700

    Debugging moment results

--- streamlit_app/pages/02-Try_Random_Polygons.py
+++ streamlit_app/pages/02-Try_Random_Polygons.py
@@ -110,6 +110,7 @@ simple_beam.def_support('N2', 1, 1, 1, 1, 0, 0)
 # Add a uniform load of 200 lbs/ft to the beam
 for dist_load in dist_loads:
     left, right = dist_load
+    st.write(left, right)
     simple_beam.add_member_dist_load('M1', 'Fy', -left[1]/1000, -right[1]/1000, left[0], right[0],   case="Case 1")
 
 # Provide simple supports
